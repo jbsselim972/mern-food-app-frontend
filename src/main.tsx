fced74c37502703b02065c2ba515710f9ede02f7
jbsselim <jbsselim@gmail.com>
2024-03-20 13:03:37 -0400

    init

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+
+import "./global.css";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "./components/ui/sonner";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+          <Toaster visibleToasts={1} position="top-right" richColors={true} />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>
+);
