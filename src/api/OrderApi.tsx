51b8b6028bfa6ba4dfb38ad3ddc2eaf2228438eb
jbsselim <jbsselim@gmail.com>
2024-04-15 23:32:50 -0400

    added get user orders hook to fetch user orders

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -1,6 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
+
+import { Order } from "@/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -18,6 +20,37 @@ type CheckoutSessionRequest = {
     country: string;
   };
   restaurantId: string;
+};
+
+export const useGetMyOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyOrdersRequest = async (): Promise<Order[]> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to get orders");
+    }
+
+    return response.json();
+  };
+
+  const { data: orders, isLoading } = useQuery(
+    "fetchMyOrders",
+    getMyOrdersRequest,
+    {
+      refetchInterval: 5000,
+    }
+  );
+
+  return { orders, isLoading };
 };
 
 export const useCreateCheckoutSession = () => {
