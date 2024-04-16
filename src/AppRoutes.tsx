063d8a01079ba3537858e8fd62433ca46537f816
jbsselim <jbsselim@gmail.com>
2024-04-15 23:33:09 -0400

    added Order status page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 
 const AppRoutes = () => {
   return (
@@ -54,6 +55,14 @@ const AppRoutes = () => {
           }
         />
       </Route>
+      <Route
+        path="/order-status"
+        element={
+          <Layout showHero={false}>
+            <OrderStatusPage />
+          </Layout>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
